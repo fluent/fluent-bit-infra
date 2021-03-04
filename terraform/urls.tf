5b69df22087496ff3938845d02a1492ebd297ef5
Jorge Niedbalski <jnr@metaklass.org>
2021-03-04 15:14:09 -0300

    switch fluentbit.io domain to proxied
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- terraform/urls.tf
+++ terraform/urls.tf
@@ -1,7 +1,7 @@
 # Redirect fluentbit.io/documentation to docs.fluentbit.io
 resource "cloudflare_page_rule" "docs-fluentbitio" {
   zone_id  = lookup(data.cloudflare_zones.fluentbit-io-zone.zones[0], "id")
-  target   = "https://${var.cloudflare_domain}/documentation"
+  target   = "https://${var.cloudflare_domain}/documentation*"
   priority = 1
 
   actions {
