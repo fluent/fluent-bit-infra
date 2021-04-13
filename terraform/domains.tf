f23c4ff1d4eb72f277ef7a9aa6cad07c93d49b12
Jorge Niedbalski <jnr@metaklass.org>
2021-04-13 12:34:53 -0400

    workflows: enforce github branch rules
    * Add github branch rules for:
       * Dismiss stale PR(s)
       * Enforce code reviewer +1
       * Required +1 before merging
    
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -70,18 +70,18 @@ resource "cloudflare_record" "apt" {
 
 resource "cloudflare_record" "config-validator-dev" {
   zone_id = lookup(data.cloudflare_zones.fluentbit-io-zone.zones[0], "id")
-  name = "config-validator-dev"
-  type = "TXT"
-  value = "google-site-verification=3LKmK6H60ZzDiqsRV_gzyMZUPBLyo_8spm020ec0wTc"
-  ttl = 3600
+  name    = "config-validator-dev"
+  type    = "TXT"
+  value   = "google-site-verification=3LKmK6H60ZzDiqsRV_gzyMZUPBLyo_8spm020ec0wTc"
+  ttl     = 3600
 }
 
 resource "cloudflare_record" "config-validator" {
   zone_id = lookup(data.cloudflare_zones.fluentbit-io-zone.zones[0], "id")
-  name = "config-validator"
-  type = "TXT"
-  value = "google-site-verification=1TdzXCdR-l4olN5FH32pJeLp4jygGM5DXJo3YgS0L20"
-  ttl = 3600
+  name    = "config-validator"
+  type    = "TXT"
+  value   = "google-site-verification=1TdzXCdR-l4olN5FH32pJeLp4jygGM5DXJo3YgS0L20"
+  ttl     = 3600
 }
 
 resource "cloudflare_record" "docs" {
---
+++ terraform/github.tf
@@ -0,0 +1,24 @@
+# Configure the GitHub Provider
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+  organization = "fluent"
+}
+
+data "github_repository" "fluentbit" {
+  full_name = "fluent/fluent-bit"
+}
+
+resource "github_branch_protection_v3" "default-branch-protection" {
+
+  repository = data.github_repository.fluentbit.full_name
+  branch     = data.github_repository.fluentbit.default_branch
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+
+  depends_on = [data.github_repository.fluentbit]
+}
