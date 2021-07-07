fde12247374d01c95bf3b151f6ac01d2242e5bf3
Jorge Niedbalski <jnr@metaklass.org>
2021-07-07 14:11:36 -0400

    github: enforce repository rules to admins.
    * Enforce repo rules to admins
    * Enforce status checks for unit/commits.
    
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- terraform/github.tf
+++ terraform/github.tf
@@ -4,13 +4,19 @@ data "github_repository" "fluentbit" {
 
 resource "github_branch_protection_v3" "default-branch-protection" {
 
-  repository = data.github_repository.fluentbit.name
-  branch     = data.github_repository.fluentbit.default_branch
+  repository     = data.github_repository.fluentbit.name
+  branch         = data.github_repository.fluentbit.default_branch
+  enforce_admins = true
 
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
     require_code_owner_reviews      = true
     required_approving_review_count = 1
+  }
+
+  required_status_checks {
+    strict   = true
+    contexts = ["Unit tests (matrix)", "Check Commit Message"]
   }
 
   depends_on = [data.github_repository.fluentbit]
