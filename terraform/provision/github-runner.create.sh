5665d448647416b7b9cdaae8e87125657d9693d2
Pat <pat@calyptia.com>
2021-12-24 16:14:55 +0000

    terraform: add ubuntu labels (#27)
    * terraform: add ubuntu labels
    * terraform: add connection
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- terraform/provision/github-runner.create.sh
+++ terraform/provision/github-runner.create.sh
@@ -43,7 +43,7 @@ pushd "$ACTIONS_RUNNER_INSTALL_DIR" > /dev/null
 
     ./config.sh --unattended --replace \
         --name "$HOSTNAME" \
-        --labels "calyptia,$RUNNER_LABEL"\
+        --labels "calyptia,ubuntu-latest,ubuntu-20.04,$RUNNER_LABEL"\
         --work "$ACTIONS_RUNNER_WORK_DIR" \
         --url "https://github.com/${GH_OWNER}/${GH_REPO}" \
         --token "$ACTIONS_RUNNER_INPUT_TOKEN"
