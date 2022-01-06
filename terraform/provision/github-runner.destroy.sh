9ebb5229666ea4ce554cc8094e5fbd707b57cd57
Patrick Stephens <pat@calyptia.com>
2022-01-06 11:14:07 +0000

    github-runners: ensure destroy always completes
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- terraform/provision/github-runner.destroy.sh
+++ terraform/provision/github-runner.destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 # Based on https://github.com/buildpacks/ci/blob/main/gh-runners/rhel-openshift/provision-scripts/github-runner.destroy.sh
 exec &> >(tee -a "/tmp/github-runner.destroy.sh.log")
-set -ex
+set -x
 
 if [ "$EUID" -eq 0 ]; then
     echo "Must be ran as a non-root user"
@@ -21,19 +21,20 @@ export RUNNER_ALLOW_RUNASROOT="1"
 ACTIONS_RUNNER_INSTALL_DIR="${HOME}/runner-${GH_REPO}"
 ACTIONS_RUNNER_WORK_DIR="${ACTIONS_RUNNER_INSTALL_DIR}-work"
 
-pushd "$ACTIONS_RUNNER_INSTALL_DIR" > /dev/null
+# Prevent destroy failures if we never provisioned initially
+pushd "$ACTIONS_RUNNER_INSTALL_DIR" || exit 0
     echo "> Stopping service..."
-    sudo ./svc.sh stop || true
+    sudo ./svc.sh stop
 
     echo "> Uninstalling service..."
-    sudo ./svc.sh uninstall || true
+    sudo ./svc.sh uninstall
 
     echo "> Unregistering runner..."
     ACTIONS_RUNNER_INPUT_TOKEN=$(curl -sS -X POST -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${GH_REPO}/actions/runners/remove-token" --header "authorization: Bearer ${GH_TOKEN}" | jq -r .token)
     echo "Token: $ACTIONS_RUNNER_INPUT_TOKEN"
 
     ./config.sh remove --unattended --token "$ACTIONS_RUNNER_INPUT_TOKEN"
+popd || true
 
-    echo "> Cleaning up..."
-    rm -rf "$ACTIONS_RUNNER_INSTALL_DIR" "$ACTIONS_RUNNER_WORK_DIR"
-popd > /dev/null
+echo "> Cleaning up..."
+rm -rf "$ACTIONS_RUNNER_INSTALL_DIR" "$ACTIONS_RUNNER_WORK_DIR"
