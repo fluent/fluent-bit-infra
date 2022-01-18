b86bd3b6925eba15c3c499e3c5095c61776d4051
Patrick Stephens <pat@calyptia.com>
2022-01-18 11:59:02 +0000

    terraform: add github mirror
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,11 +1,32 @@
 data "github_repository" "fluentbit" {
   full_name = var.repo_full_name
 }
-resource "github_branch_protection_v3" "default-branch-protection" {
+resource "github_repository" "fluent-bit-mirror" {
+  name        = "fluent-bit-mirror"
+  description = "A private mirror of Fluent Bit purely to mitigate security concerns of using self-hosted runners."
 
-  repository     = data.github_repository.fluentbit.name
-  branch         = data.github_repository.fluentbit.default_branch
+  visibility = "private"
+
+  delete_branch_on_merge = true
+  archive_on_destroy     = true
+}
+
+resource "github_branch" "fluent-bit-mirror" {
+  repository = github_repository.fluent-bit-mirror.name
+  branch     = data.github_repository.fluentbit.default_branch
+}
+
+locals {
+  fluent-bit-repos = [ data.github_repository.fluentbit, github_repository.fluent-bit-mirror ]
+}
+resource "github_branch_protection_v3" "default-branch-protection" {
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository     = each.value.name
+  branch         = each.value.default_branch
   enforce_admins = false
+
+  require_signed_commits = true
 
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
@@ -17,8 +38,6 @@ resource "github_branch_protection_v3" "default-branch-protection" {
     strict   = false
     contexts = ["Unit tests (matrix)", "Check Commit Message"]
   }
-
-  depends_on = [data.github_repository.fluentbit]
 }
 
 data "github_user" "release-approvers-users" {
@@ -27,8 +46,10 @@ data "github_user" "release-approvers-users" {
 }
 
 resource "github_repository_environment" "release-environment" {
-  environment  = "release"
-  repository   = data.github_repository.fluentbit.name
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  environment = "release"
+  repository  = each.value.name
   reviewers {
     users = [ for user in data.github_user.release-approvers-users: user.id ]
   }
@@ -36,52 +57,60 @@ resource "github_repository_environment" "release-environment" {
     protected_branches     = true
     custom_branch_policies = false
   }
-
-  depends_on = [data.github_repository.fluentbit]
 }
 
 resource "github_actions_environment_secret" "release-bucket-secret" {
-  repository       = data.github_repository.fluentbit.name
-  environment      = github_repository_environment.release-environment.environment
-  secret_name      = "AWS_S3_BUCKET_RELEASE"
-  plaintext_value  = var.release-s3-bucket
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository      = each.value.name
+  environment     = github_repository_environment.release-environment[each.key].environment
+  secret_name     = "AWS_S3_BUCKET_RELEASE"
+  plaintext_value = var.release-s3-bucket
 }
 
 resource "github_repository_environment" "staging-environment" {
-  environment  = "staging"
-  repository   = data.github_repository.fluentbit.name
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  environment = "staging"
+  repository  = each.value.name
   deployment_branch_policy {
     protected_branches     = true
     custom_branch_policies = false
   }
-
-  depends_on = [data.github_repository.fluentbit]
 }
 
 resource "github_actions_environment_secret" "staging-bucket-secret" {
-  repository       = data.github_repository.fluentbit.name
-  environment      = github_repository_environment.staging-environment.environment
-  secret_name      = "AWS_S3_BUCKET_STAGING"
-  plaintext_value  = var.staging-s3-bucket
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository      = each.value.name
+  environment     = github_repository_environment.staging-environment[each.key].environment
+  secret_name     = "AWS_S3_BUCKET_STAGING"
+  plaintext_value = var.staging-s3-bucket
 }
 
 resource "github_actions_environment_secret" "staging-aws-access-key-id-secret" {
-  repository       = data.github_repository.fluentbit.name
-  environment      = github_repository_environment.staging-environment.environment
-  secret_name      = "AWS_ACCESS_KEY_ID"
-  plaintext_value  = var.staging-s3-access-id
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository      = each.value.name
+  environment     = github_repository_environment.staging-environment[each.key].environment
+  secret_name     = "AWS_ACCESS_KEY_ID"
+  plaintext_value = var.staging-s3-access-id
 }
 
 resource "github_actions_environment_secret" "staging-aws-secret-access-key-secret" {
-  repository       = data.github_repository.fluentbit.name
-  environment      = github_repository_environment.staging-environment.environment
-  secret_name      = "AWS_SECRET_ACCESS_KEY"
-  plaintext_value  = var.staging-s3-secret-access-key
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository      = each.value.name
+  environment     = github_repository_environment.staging-environment[each.key].environment
+  secret_name     = "AWS_SECRET_ACCESS_KEY"
+  plaintext_value = var.staging-s3-secret-access-key
 }
 
 resource "github_actions_environment_secret" "staging-gpg-private-key-secret" {
-  repository       = data.github_repository.fluentbit.name
-  environment      = github_repository_environment.staging-environment.environment
-  secret_name      = "GPG_PRIVATE_KEY"
-  plaintext_value  = var.staging-gpg-key
+  for_each = { for repo in local.fluent-bit-repos: repo.name => repo }
+
+  repository      = each.value.name
+  environment     = github_repository_environment.staging-environment[each.key].environment
+  secret_name     = "GPG_PRIVATE_KEY"
+  plaintext_value = var.staging-gpg-key
 }
