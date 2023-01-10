b8d421f13b79a991efa77cabf42ec5eff2e25115
Pat <pat@calyptia.com>
2023-01-10 11:44:01 +0000

    machines: switch to file for user-data (#160)
    Signed-off-by: Patrick Stephens <pat@calyptia.com>
    
    Signed-off-by: Patrick Stephens <pat@calyptia.com>

--- terraform/provision/package-server-provision.sh
+++ terraform/provision/package-server-provision.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
-set -eu
-mkdir -p /var/www/apt.fluentbit.io /var/www/releases.fluentbit.io
+#!/bin/env bash
+exec &> >(tee -a "/tmp/user-data.sh.log")
+set -ex
 
+echo "> Creating non-root user..."
+adduser --ingroup sudo --disabled-password --gecos "" provisioner
+# allow for PATH to persist
+sed -i '/Defaults \+secure_path/s/^/#/' /etc/sudoers
+# do not require password
+sed -i '0,/%sudo/s/ALL$/NOPASSWD: ALL/' /etc/sudoers
+
+echo "> Updating and installing dependencies"
+# Add dependencies and allow for other users of apt-get
+# https://blog.sinjakli.co.uk/2021/10/25/waiting-for-apt-locks-without-the-hacky-bash-scripts/
+apt-get -o DPkg::Lock::Timeout=60 update
+apt-get -o DPkg::Lock::Timeout=-1 install -y docker.io nginx awscli git
+usermod -aG docker provisioner
+
+echo "> Configuring Nginx"
+mkdir -p /var/www/apt.fluentbit.io /var/www/releases.fluentbit.io
 
 # Set up the package serving
 cat > /etc/nginx/sites-available/apt.fluentbit.io <<'EOF'
@@ -65,6 +81,7 @@ systemctl restart nginx
 # aws s3 sync s3://fluentbit-releases /var/www/apt.fluentbit.io --no-sign-request
 
 # Set up cron job
+echo "> Setting up S3 sync job"
 cat > /etc/systemd/system/packages-sync.service <<EOF
 [Unit]
 Description=Syncs all release packages from the AWS bucket to this server.
@@ -94,6 +111,9 @@ EOF
 systemctl daemon-reload
 systemctl enable --now packages-sync.timer
 
+echo "> Deploying metrics exporter"
 mkdir -p /opt/fluent-bit-stats
 git clone https://github.com/niedbalski/fluent-bit-stats.git /opt/fluent-bit-stats
 ( cd /opt/fluent-bit-stats; docker compose up -d )
+
+echo "Completed provisioning successfully"
